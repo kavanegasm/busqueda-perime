b1a07a0a288d170e862ff0a1581e946bd894fdd2
kigama <kavanegasm@unal.edu.co>
2020-04-12 22:36:10 -0500

    Update main.go

--- main.go
+++ main.go
@@ -2,252 +2,230 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"math/rand"
-	"time"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
-	"github.com/gorilla/mux"
+	"strconv"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
-//mux
-type Person struct {
-	ID string `json:"id,omitempty"`
-	FirstName string `json:"firstname,omitempty"`
-	LastName string `json:"lastname,omitempty"`
-	Address *Address `json:"address,omitempty"`
-  }
-  
-  type Address struct {
-	City string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-  }
-  
-  var people []Person
-  
-  // EndPoints
-  func GetPersonEndpoint(w http.ResponseWriter, req *http.Request){
-	params := mux.Vars(req)
-	for _, item := range people {
-	  if item.ID == params["id"] {
-		json.NewEncoder(w).Encode(item)
-		return
-	  }
-	}
-	json.NewEncoder(w).Encode(&Person{})
-  }
-  
-  func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request){
-	json.NewEncoder(w).Encode(people)
-  }
-  
-  func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request){
-	params := mux.Vars(req)
-	var person Person
-	_ = json.NewDecoder(req.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
-  
-  }
-  
-  func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	for index, item := range people {
-	  if item.ID == params["id"] {
-		people = append(people[:index], people[index + 1:]...)
-		break
-	  }
-	}
-	json.NewEncoder(w).Encode(people)
-  }
-// Categoria es una entrada de contenido.
-type Categoria struct {
-	Id     int
-	CategoriaId int
-	NombreCategoria  string
-	TipoCategoria   string
-}
-//publico
-// Get busca un categoria por ID. El bool es falso si no lo encontramos.
-func Get(id int) (Categoria, bool) {
-	categorias := getCategorias(id)
-	if len(categorias) == 0 {
-		// Slice vacío; no se encontró el categoria.
-		return Categoria{}, false
-	}
-	return categorias[0], true
+
+//main
+func main() {
+	a := App{} 
+    // You need to set your Username and Password here
+    a.Initialize("root", "password", "busqueda-db")
+
+    a.Run(":50000")
+
 }
 
-// List devuelve un slice de todos los categorias.
-func List() []Categoria {
-	return getCategorias(-1)
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
 }
 
-// New guarda un categoria nuevo.
-func New(p Categoria) []Categoria {
-	return newCategoria(p)
-}
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbname)
 
-// Put guarda un categoria existente.
-func Put(p Categoria) {
-	putCategoria(p)
-}
-
-// Del borra un categoria.
-func Del(id int) {
-	delCategoria(id)
-}
-
-// db es la base de datos global
-var db *sql.DB
-
-// Prepared statements
-type stmtConfig struct {
-	stmt *sql.Stmt
-	q    string
-}
-
-var prepStmts = map[string]*stmtConfig{
-	"get":    {q: "select * from categoria where id = ?;"},
-	"list":   {q: "select * from categoria;"},
-	"insert": {q: "insert into categoria (id, categoriaId, nombreCategoria, tipoCategoria) values (?, ?, ?, ?);"},
-	"update": {q: "update categoria set categoriaId = ?, nombreCategoria = ?, tipoCategoria = ? where id = ?;"},
-	"delete": {q: "delete from categoria where id = ?;"},
-}
-
-//privado 
-
-// getCategorias busca un categoria con id o listado de todos si id es -1.
-func getCategorias(id int) []Categoria {
-	res := []Categoria{}
-	if id != -1 {
-		var p Categoria
-		// Obtenemos y ejecutamos el get prepared statement.
-		get := prepStmts["get"].stmt
-		err := get.QueryRow(id).Scan(&p.Id, &p.CategoriaId , &p.NombreCategoria, &p.TipoCategoria )
-		if err != nil {
-			if err != sql.ErrNoRows {
-				log.Printf("categoria: error getting categoria. Id: %d, err: %v\n", id, err)
-			}
-		} else {
-			res = append(res, p)
-		}
-		return res
-	}
-
-	// Obtenemos y ejecutamos el list prepared statement.
-	list := prepStmts["list"].stmt
-	rows, err := list.Query()
+	var err error
+	a.DB, err = sql.Open("mysql", connectionString)
 	if err != nil {
-		log.Printf("categoria: error getting categorias. err: %v\n", err)
+		log.Fatal(err)
 	}
+
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+}
+
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/categorias", a.getCategorias).Methods("GET")
+	a.Router.HandleFunc("/categoria", a.createCategoria).Methods("POST")
+	a.Router.HandleFunc("/categoria/{id:[0-9]+}", a.getCategoria).Methods("GET")
+	a.Router.HandleFunc("/categoria/{id:[0-9]+}", a.updateCategoria).Methods("PUT")
+	a.Router.HandleFunc("/categoria/{id:[0-9]+}", a.deleteCategoria).Methods("DELETE")
+}
+
+func (a *App) getCategorias(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+	if start < 0 {
+		start = 0
+	}
+
+	products, err := getCategorias(a.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, products)
+}
+
+func (a *App) createCategoria(w http.ResponseWriter, r *http.Request) {
+	var u categoria
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&u); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := u.createCategoria(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, u)
+}
+
+func (a *App) getCategoria(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid categoria ID")
+		return
+	}
+
+	u := categoria{ID: id}
+	if err := u.getCategoria(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Categoria not found press f")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, u)
+}
+
+func (a *App) updateCategoria(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid categoria ID")
+		return
+	}
+
+	var u categoria
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&u); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+		return
+	}
+	defer r.Body.Close()
+	u.ID = id
+
+	if err := u.updateCategoria(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, u)
+}
+
+func (a *App) deleteCategoria(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Categoria ID")
+		return
+	}
+
+	u := categoria{ID: id}
+	if err := u.deleteCategoria(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+
+//model
+type categoria struct {
+	ID   int    `json:"id"`
+	NombreCategoria  string `json:"nombrecategoria"`
+	TipoCategoria   string `json:"tipocategoria"`
+}
+
+func (u *categoria) getCategoria(db *sql.DB) error {
+	statement := fmt.Sprintf("SELECT NombreCategoria, TipoCategoria FROM categorias WHERE id=%d", u.ID)
+	return db.QueryRow(statement).Scan(&u.NombreCategoria, &u.TipoCategoria)
+}
+
+func (u *categoria) updateCategoria(db *sql.DB) error {
+	statement := fmt.Sprintf("UPDATE categorias SET NombreCategoria='%s', TipoCategoria='%s' WHERE id=%d", u.NombreCategoria, u.TipoCategoria, u.ID)
+	_, err := db.Exec(statement)
+	return err
+}
+
+func (u *categoria) deleteCategoria(db *sql.DB) error {
+	statement := fmt.Sprintf("DELETE FROM categorias WHERE id=%d", u.ID)
+	_, err := db.Exec(statement)
+	return err
+}
+
+func (u *categoria) createCategoria(db *sql.DB) error {
+	statement := fmt.Sprintf("INSERT INTO categorias(NombreCategoria, TipoCategoria) VALUES('%s', '%s')", u.NombreCategoria, u.TipoCategoria)
+	_, err := db.Exec(statement)
+
+	if err != nil {
+		return err
+	}
+
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&u.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getCategorias(db *sql.DB, start, count int) ([]categoria, error) {
+	statement := fmt.Sprintf("SELECT id, NombreCategoria, TipoCategoria FROM categorias LIMIT %d OFFSET %d", count, start)
+	rows, err := db.Query(statement)
+
+	if err != nil {
+		return nil, err
+	}
+
 	defer rows.Close()
 
-	// Procesamos los rows.
+	categorias := []categoria{}
+
 	for rows.Next() {
-		var p Categoria
-		if err := rows.Scan(&p.Id, &p.CategoriaId , &p.NombreCategoria, &p.TipoCategoria ); err != nil {
-			log.Printf("categoria: error scanning row: %v\n", err)
-			continue
+		var u categoria
+		if err := rows.Scan(&u.ID, &u.NombreCategoria, &u.TipoCategoria); err != nil {
+			return nil, err
 		}
-		res = append(res, p)
-	}
-	// Verificamos si hubo error procesando los rows.
-	if err := rows.Err(); err != nil {
-		log.Printf("categoria: error reading rows: %v\n", err)
+		categorias = append(categorias, u)
 	}
 
-	return res
-}
-
-// newCategoria inserta un categoria en la DB.
-func newCategoria(p Categoria) []Categoria {
-	// Generamos ID único para el nuevo categoria.
-	p.Id = rand.Intn(1000)
-	for {
-		l := getCategorias(p.Id)
-		if len(l) == 0 {
-			break
-		}
-		p.Id = rand.Intn(1000)
-	}
-
-	// Obtenemos y ejecutamos insert prepared statement.
-	insert := prepStmts["insert"].stmt
-	_, err := insert.Exec(p.Id, p.CategoriaId , p.NombreCategoria, p.TipoCategoria )
-	if err != nil {
-		log.Printf("categoria: error inserting categoria %d into DB: %v\n", p.Id, err)
-	}
-	return []Categoria{p}
-}
-
-// putCategoria actualiza un categoria en la DB.
-func putCategoria(p Categoria) {
-	// Obtenemos y ejecutamos update prepared statement.
-	update := prepStmts["update"].stmt
-	_, err := update.Exec(p.CategoriaId , p.NombreCategoria, p.TipoCategoria , p.Id)
-	if err != nil {
-		log.Printf("categoria: error updating categoria %d into DB: %v\n", p.Id, err)
-	}
-}
-
-// delCategoria borra un categoria de la DB.
-func delCategoria(id int) {
-	// Obtenemos y ejecutamos delete prepared statement.
-	del := prepStmts["delete"].stmt
-	_, err := del.Exec(id)
-	if err != nil {
-		log.Printf("categoria: error deleting categoria %d into DB: %v\n", id, err)
-	}
-}
-
-func main() {
-	// Open database connection
-	//db, err := sql.Open("mysql", "root:password@/busqueda-db")
-	rand.Seed(time.Now().UnixNano())
-
-	// Info para la DB.
-	const (
-		driver       = "mysql"
-		dsn          = "busqueda-db"
-		categoriaTableSQL  = `create table if not exists post(
-			id int primary key not null,
-			CategoriaId int not null,
-			NombreCategoria text not null,
-			TipoCategoria text not null
-		);`
-	)
-
-	// Abrimos la base de datos
-	var err error
-	//db, err = sql.Open(driver, dsn)
-	db, err := sql.Open("mysql", "root:password@/busqueda-db")
-        fmt.Println("hello world",db)
-
-	if err != nil {
-		panic(err.Error()) 
-	}
-
-	// Creamos la tabla para loas categorias, si no existe
-	_, err = db.Exec(categoriaTableSQL )
-	if err != nil {
-		panic(err.Error()) 
-	}
-	//definimos como escuchar
-	 router := mux.NewRouter()
-  
-	// data ejemplo
-  	people = append(people, Person{ID: "1", FirstName:"Ryan", LastName:"Ray", Address: &Address{City:"Dubling", State:"California"}})
-  	people = append(people, Person{ID: "2", FirstName:"Maria", LastName:"Ray"})
-
-	// endpoints
-	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
-
-	log.Fatal(http.ListenAndServe(":1859", router))
-
+	return categorias, nil
 }
